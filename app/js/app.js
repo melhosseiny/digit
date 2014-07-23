885c7926f61623ea511e8e37e273b23903755d6b
Mostafa Elshamy <melhosseiny@server.fake>
2014-07-22 22:12:43 -0700

    Fix: Menu functionality and timer
    - Add function to generate a new puzzle
    - Check if puzzle was solved by man or machine
    - Reduce amount of calls to search function

--- app/js/app.js
+++ app/js/app.js
@@ -58,24 +58,45 @@ digit.controller('DigitCtrl', ['$scope', '$timeout', 'S', function DigitCtrl($sc
   $scope.elapsed_time = 0;
 
   $scope.init_puzzle = parse_grid_no_cp(S.wikigrid);
-  $scope.puzzle = $scope.init_puzzle;
+  $scope.puzzle = angular.copy($scope.init_puzzle);
 
   $scope.active_peers = [];
+  var timeout_id;
+
+  $scope.new_puzzle = function() {
+    $scope.init_puzzle = parse_grid_no_cp(random_puzzle());
+    $scope.puzzle = angular.copy($scope.init_puzzle);
+    $scope.init_moment = moment();
+    $scope.tick();
+  }
 
   $scope.tick = function() {
     var diff = moment().diff($scope.init_moment);
     var duration = moment.duration(diff);
     $scope.elapsed_time = duration.hours() + ":" + duration.minutes() + ":" + duration.seconds();
-    $timeout($scope.tick, 1000);
+    timeout_id = $timeout($scope.tick, 1000);
   }
 
   $scope.solve = function() {
     $scope.puzzle = search(parse_grid(to_grid($scope.puzzle)));
+    $scope.solved_by_machine = true;
+    $timeout.cancel(timeout_id);
+  }
+
+  $scope.is_filled = function() {
+    return _.every($scope.puzzle, function(v, k) {
+      return v.length === 1;
+    })
   }
 
   $scope.check = function() {
-    $scope.solution = search(parse_grid(to_grid($scope.puzzle)));
-    return to_grid($scope.puzzle) === to_grid($scope.solution);
+    if ($scope.is_filled()) {
+      $scope.solution = search(parse_grid(to_grid($scope.puzzle)));
+      if (to_grid($scope.puzzle) === to_grid($scope.solution)) {
+        $timeout.cancel(timeout_id);
+        return true;
+      }
+    }
   }
 
   $scope.rotate = function(square) {
@@ -87,12 +108,12 @@ digit.controller('DigitCtrl', ['$scope', '$timeout', 'S', function DigitCtrl($sc
       }), function(v) {
         return v.length === 1;
       }));
-
-      var next = 1 + (($scope.puzzle[square]) % 9);
+      var next = $scope.puzzle[square].length > 1? 1 :
+        1 + ((+($scope.puzzle[square])) % 9);
       while (omit.indexOf(next.toString()) != -1) {
         next = 1 + (next % 9);
       }
-      assign_no_cp($scope.puzzle, square, next);
+      assign_no_cp($scope.puzzle, square, next.toString());
     }
   }
 
